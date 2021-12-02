3972ec4a9a25de67a402ff1df8c09beacd64fc1d
EunhaKyeong <kyeongeh424@gmail.com>
2021-12-03 02:31:19 +0900

    Design: 추가-MainActivity 디자인

--- app/src/main/java/com/pProject/ganada/MainActivity.java
+++ app/src/main/java/com/pProject/ganada/MainActivity.java
@@ -1,14 +1,37 @@
 package com.pProject.ganada;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View takePictureView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        takePictureView = (View) findViewById(R.id.take_picture_view);
+
+        takePictureView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startDialog();
+            }
+        });
+    }
+
+    private void startDialog() {
+        Dialog dialog = new Dialog(this);       // Dialog 초기화
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE); // 타이틀 제거
+        dialog.setContentView(R.layout.take_picture_dialog);    // xml 레이아웃 파일과 연결
+
+        dialog.show();
     }
 }
