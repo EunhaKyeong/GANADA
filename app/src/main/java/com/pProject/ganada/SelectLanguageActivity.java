03d829cf4131f4a0fd99d8c739d960073a9fa918
EunhaKyeong <kyeongeh424@gmail.com>
2021-12-03 02:33:14 +0900

    Design: 추가 - SelectLanguageActivity 디자인

---
+++ app/src/main/java/com/pProject/ganada/SelectLanguageActivity.java
@@ -0,0 +1,32 @@
+package com.pProject.ganada;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SelectLanguageActivity extends AppCompatActivity {
+
+    private Button settingUpBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_language);
+
+        settingUpBtn = (Button) findViewById(R.id.setting_up_btn);
+        settingUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goMainActivity();
+            }
+        });
+
+    }
+
+    public void goMainActivity() {
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+    }
+}
