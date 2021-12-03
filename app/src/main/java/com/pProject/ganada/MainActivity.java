9c12feb47bfcee3dfe6d1954e10f3cb6844501bb
EunhaKyeong <kyeongeh424@gmail.com>
2021-12-04 00:29:48 +0900

    Feat: 추가 - sharedPreferences 에서 language 데이터 가져오기, language에 따라 UI 업데이트

--- app/src/main/java/com/pProject/ganada/MainActivity.java
+++ app/src/main/java/com/pProject/ganada/MainActivity.java
@@ -12,17 +12,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView takePictureTv, vocaTv, settingTv;
     private View takePictureView;
     private Dialog dialog;
+    private String language;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //sharedPreferences 에서 선택된 언어 가져오기
+        language = getSharedPreferences("Language", MODE_PRIVATE).getString("language", null);
+
+        setLanguageUI(language);    //선택된 언어에 맞춰 TextView 의 텍스트를 설정하는 함수 호출
 
         //이미지/텍스트 촬영 클릭 리스너 -> 물체 인식인지 텍스트 인식인지 클릭하는 다이얼로그 띄우기
         takePictureView = (View) findViewById(R.id.take_picture_view);
@@ -47,11 +55,45 @@ public class MainActivity extends AppCompatActivity {
         dialog.dismiss();   //다이얼로그 닫기
     }
 
+    //선택된 언어에 맞춰 TextView 의 텍스트를 설정하는 함수
+    private void setLanguageUI(String language) {
+
+        takePictureTv = (TextView) findViewById(R.id.take_picture_tv_other_language);
+        vocaTv = (TextView) findViewById(R.id.voca_other_language_tv);
+        settingTv = (TextView) findViewById(R.id.setting_other_language_tv);
+
+        switch (language) {
+            case "english":
+                takePictureTv.setText(R.string.take_picture_en);
+                vocaTv.setText(R.string.vocabulary_en);
+                settingTv.setText(R.string.setting_en);
+                break;
+            case "china":
+                takePictureTv.setText(R.string.take_picture_cn);
+                vocaTv.setText(R.string.vocabulary_cn);
+                settingTv.setText(R.string.setting_cn);
+                break;
+            case "vietnam":
+                takePictureTv.setText(R.string.take_picture_vn);
+                vocaTv.setText(R.string.vocabulary_vn);
+                settingTv.setText(R.string.setting_vn);
+                break;
+            default:
+                takePictureTv.setText(R.string.take_picture_jp);
+                vocaTv.setText(R.string.vocabulary_jp);
+                settingTv.setText(R.string.setting_jp);
+                break;
+        }
+    }
+
+    //이미지/텍스트 촬영 다이얼로그 띄우는 함수
     private void startDialog() {
         dialog = new Dialog(this);  // Dialog 초기화
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE); // 타이틀 제거
         dialog.setContentView(R.layout.take_picture_dialog);    // xml 레이아웃 파일과 연결
         dialog.show();
+
+        setDialogLanguageUI(language);  //선택된 언어에 맞춰 다이얼로그 텍스트뷰의 텍스트 언어를 설정하는 함수 호출
 
         //사진 찍기 클릭 리스너 -> 카메라 실행 로직 함수 호출
         View objectRecognitionView = (View) dialog.findViewById(R.id.object_recognition_view);
@@ -79,6 +121,37 @@ public class MainActivity extends AppCompatActivity {
                 dialog.dismiss();
             }
         });
+    }
+
+    //선택된 언어에 맞춰 다이얼로그 텍스트뷰의 텍스트 언어를 설정하는 함수
+    private void setDialogLanguageUI(String language) {
+
+        TextView alertTv = (TextView) dialog.findViewById(R.id.alert_other_language_tv);
+        TextView objectRecognitionTv = (TextView) dialog.findViewById(R.id.object_recognition_other_language_tv);
+        TextView textRecognitionTv = (TextView) dialog.findViewById(R.id.text_recognition_other_language_tv);
+
+        switch (language) {
+            case "english":
+                alertTv.setText(R.string.take_photo_alert_en);
+                objectRecognitionTv.setText(R.string.object_recognition_en);
+                textRecognitionTv.setText(R.string.text_recognition_en);
+                break;
+            case "china":
+                alertTv.setText(R.string.take_photo_alert_cn);
+                objectRecognitionTv.setText(R.string.object_recognition_cn);
+                textRecognitionTv.setText(R.string.text_recognition_cn);
+                break;
+            case "vietnam":
+                alertTv.setText(R.string.take_photo_alert_vn);
+                objectRecognitionTv.setText(R.string.object_recognition_vn);
+                textRecognitionTv.setText(R.string.text_recognition_vn);
+                break;
+            default:
+                alertTv.setText(R.string.take_photo_alert_jp);
+                objectRecognitionTv.setText(R.string.object_recognition_jp);
+                textRecognitionTv.setText(R.string.text_recognition_jp);
+                break;
+        }
     }
 
     //카메라 실행 로직 함수
