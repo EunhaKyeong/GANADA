0079b35480356031cf8cb00638b0d7a4f136a005
EunhaKyeong <kyeongeh424@gmail.com>
2021-12-06 09:33:13 +0900

    Comment: 주석 삭제

--- app/src/main/java/com/pProject/ganada/SelectLanguageActivity.java
+++ app/src/main/java/com/pProject/ganada/SelectLanguageActivity.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
