525888bfa1184d9084ced911d6b80ecab0d1bff5
EunhaKyeong <kyeongeh424@gmail.com>
2021-12-08 16:55:09 +0900

    Feat: 추가-TTS 기능

--- app/src/main/java/com/pProject/ganada/LearnWordActivity.java
+++ app/src/main/java/com/pProject/ganada/LearnWordActivity.java
@@ -2,9 +2,10 @@ package com.pProject.ganada;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.Gravity;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.ImageButton;
@@ -12,12 +13,14 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+
+import java.util.Locale;
 
 public class LearnWordActivity extends AppCompatActivity {
 
+    private TextToSpeech tts;
     private TextView word, example_sentence, practice_foreign;
     private ImageButton btn_back, btn_word_pronunciation, btn_sentence_pronunciation;
     private CheckBox btn_bookmark;
@@ -30,28 +33,57 @@ public class LearnWordActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn_word);
 
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                tts.setLanguage(Locale.KOREAN);
+                tts.setSpeechRate(0.75f);
+            }
+        });
+
+        //sharedPreferences 에서 선택된 언어 가져오기
+        language = getSharedPreferences("Language", MODE_PRIVATE).getString("language", null);
+        setLanguageUI(language);    //선택된 언어에 맞춰 TextView 의 텍스트를 설정하는 함수 호출
+
         //intent 를 통해 image Uri, 인식된 텍스트를 전달 받음.
         Intent intent = getIntent();
         Uri uri = Uri.parse(intent.getStringExtra("uri"));
         String text = intent.getStringExtra("recognizedText");
+        String exam = intent.getStringExtra("exam");
 
         //전달 받은 텍스트로 UI 바인딩
         word = (TextView) findViewById(R.id.word);
         word.setText(text);
 
+        //단어 스피커 이미지버튼 클릭 리스너
+        btn_word_pronunciation = (ImageButton) findViewById(R.id.btn_word_pronunciation);
+        btn_word_pronunciation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                speakOut(word.getText().toString());
+            }
+        });
+
         //전달 받은 uri 로 UI 바인딩
         word_pic = (ImageView) findViewById(R.id.word_pic);
         word_pic.setImageURI(uri);
 
+        //전달 받은 exam 로 UI 바인딩
+        example_sentence = (TextView) findViewById(R.id.example_sentence);
+        example_sentence.setText(exam);
+
+        //예문 스피커 이미지버튼 클릭 리스너
+        btn_sentence_pronunciation = (ImageButton) findViewById(R.id.btn_sentence_pronunciation);
+        btn_sentence_pronunciation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                speakOut(example_sentence.getText().toString());
+            }
+        });
+
         btn_bookmark = (CheckBox) findViewById(R.id.btn_bookmark);
         btn_back = (ImageButton) findViewById(R.id.btn_back);
-        btn_word_pronunciation = (ImageButton) findViewById(R.id.btn_word_pronunciation);
-        btn_sentence_pronunciation = (ImageButton) findViewById(R.id.btn_sentence_pronunciation);
         practice_view = (View) findViewById(R.id.practice_view);
-
-        //sharedPreferences 에서 선택된 언어 가져오기
-        language = getSharedPreferences("Language", MODE_PRIVATE).getString("language", null);
-        setLanguageUI(language);    //선택된 언어에 맞춰 TextView 의 텍스트를 설정하는 함수 호출
 
         //뒤로가기 버튼
         btn_back.setOnClickListener(new View.OnClickListener() {
@@ -65,15 +97,15 @@ public class LearnWordActivity extends AppCompatActivity {
         btn_bookmark.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(btn_bookmark.isChecked()) {
+                if (btn_bookmark.isChecked()) {
                     Toast toast = Toast.makeText(getApplicationContext(), getString(R.string.save_word_en), Toast.LENGTH_SHORT);
                     TextView v = (TextView) toast.getView().findViewById(android.R.id.message);
-                    if( v != null) v.setGravity(Gravity.CENTER);
+                    if (v != null) v.setGravity(Gravity.CENTER);
                     toast.show();
                 } else {
                     Toast toast = Toast.makeText(getApplicationContext(), getString(R.string.delete_word_en), Toast.LENGTH_SHORT);
                     TextView v = (TextView) toast.getView().findViewById(android.R.id.message);
-                    if( v != null) v.setGravity(Gravity.CENTER);
+                    if (v != null) v.setGravity(Gravity.CENTER);
                     toast.show();
                 }
             }
@@ -86,8 +118,15 @@ public class LearnWordActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), PracticeWordActivity.class));
             }
         });
+    }
 
-
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
     }
 
     //선택된 언어에 맞춰 TextView 의 텍스트를 설정하는 함수
@@ -109,4 +148,11 @@ public class LearnWordActivity extends AppCompatActivity {
                 break;
         }
     }
+
+    //TTS 함수
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void speakOut(String text) {
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+    }
+
 }
