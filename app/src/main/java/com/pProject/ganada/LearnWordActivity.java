90138d5113733aa48e7795bed8a12c015241c4b5
jaeeunchang <cjeun8@naver.com>
2021-12-09 09:38:45 +0900

    Feat: 추가-단어 저장 및 삭제 기능 추가

--- app/src/main/java/com/pProject/ganada/LearnWordActivity.java
+++ app/src/main/java/com/pProject/ganada/LearnWordActivity.java
@@ -1,7 +1,9 @@
 package com.pProject.ganada;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.MenuItem;
@@ -14,11 +16,19 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.LinearLayoutCompat$InspectionCompanion;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 public class LearnWordActivity extends AppCompatActivity {
 
-    private TextView word, example_sentence, practice_foreign;
+    private VocaDB vocaDB = null;
+    private Context mContext;
+
+    private TextView word_tv, ex_sentence_tv, practice_foreign;
     private ImageButton btn_back, btn_word_pronunciation, btn_sentence_pronunciation;
     private CheckBox btn_bookmark;
     private ImageView word_pic;
@@ -33,6 +43,9 @@ public class LearnWordActivity extends AppCompatActivity {
         Intent intent = getIntent();
         Uri uri = Uri.parse(intent.getStringExtra("uri"));
 
+        word_tv = (TextView) findViewById(R.id.word);
+        ex_sentence_tv = (TextView) findViewById(R.id.ex_sentence);
+
         word_pic = (ImageView) findViewById(R.id.word_pic);
         word_pic.setImageURI(uri);
 
@@ -41,6 +54,10 @@ public class LearnWordActivity extends AppCompatActivity {
         btn_word_pronunciation = (ImageButton) findViewById(R.id.btn_word_pronunciation);
         btn_sentence_pronunciation = (ImageButton) findViewById(R.id.btn_sentence_pronunciation);
         practice_view = (View) findViewById(R.id.practice_view);
+
+        //DB 생성
+        vocaDB = VocaDB.getInstance(this);
+        mContext = getApplicationContext();
 
         //sharedPreferences 에서 선택된 언어 가져오기
         language = getSharedPreferences("Language", MODE_PRIVATE).getString("language", null);
@@ -54,16 +71,46 @@ public class LearnWordActivity extends AppCompatActivity {
             }
         });
 
-        //단어 저장 및 삭제시 토스트메세지 출력
+        class InsertRunnable implements Runnable {
+            @Override
+            public void run() {
+                Voca voca = new Voca();
+                voca.word = word_tv.getText().toString();
+                voca.ex_sentence = ex_sentence_tv.getText().toString();
+                voca.picture_uri = uri.toString();
+                VocaDB.getInstance(mContext).vocaDao().insert(voca);
+            }
+        }
+
+        class DeleteRunnable implements Runnable {
+            @Override
+            public void run() {
+                Voca voca = new Voca();
+                VocaDB.getInstance(mContext).vocaDao().delete(VocaDB.getInstance(mContext).vocaDao().findByUri(uri.toString()));
+            }
+        }
+
         btn_bookmark.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(btn_bookmark.isChecked()) {
+                    //insert DB
+                    InsertRunnable insertRunnable = new InsertRunnable();
+                    Thread addThread = new Thread(insertRunnable);
+                    addThread.start();
+
                     Toast toast = Toast.makeText(getApplicationContext(), getString(R.string.save_word_en), Toast.LENGTH_SHORT);
                     TextView v = (TextView) toast.getView().findViewById(android.R.id.message);
-                    if( v != null) v.setGravity(Gravity.CENTER);
+                    if(v != null) v.setGravity(Gravity.CENTER);
                     toast.show();
+
+                    btn_bookmark.setEnabled(true);
                 } else {
+                    //delete DB
+                    DeleteRunnable deleteRunnable = new DeleteRunnable();
+                    Thread deleteThread = new Thread(deleteRunnable);
+                    deleteThread.start();
+
                     Toast toast = Toast.makeText(getApplicationContext(), getString(R.string.delete_word_en), Toast.LENGTH_SHORT);
                     TextView v = (TextView) toast.getView().findViewById(android.R.id.message);
                     if( v != null) v.setGravity(Gravity.CENTER);
@@ -76,11 +123,11 @@ public class LearnWordActivity extends AppCompatActivity {
         practice_view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), PracticeWordActivity.class));
+                Intent intent = new Intent(getApplicationContext(), PracticeWordActivity.class);
+                intent.putExtra("word", word_tv.getText());
+                startActivity(intent);
             }
         });
-
-
     }
 
     //선택된 언어에 맞춰 TextView 의 텍스트를 설정하는 함수
