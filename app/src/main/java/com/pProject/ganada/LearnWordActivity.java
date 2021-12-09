c79a961aa9cc922325e7d9f11a5b396a54f2aa68
jaeeunchang <cjeun8@naver.com>
2021-12-09 10:23:05 +0900

    Design 수정: textView 이름 변경

--- app/src/main/java/com/pProject/ganada/LearnWordActivity.java
+++ app/src/main/java/com/pProject/ganada/LearnWordActivity.java
@@ -3,7 +3,6 @@ package com.pProject.ganada;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
@@ -19,12 +18,6 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Locale;
-import androidx.appcompat.widget.LinearLayoutCompat$InspectionCompanion;
-import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import java.util.List;
 
 public class LearnWordActivity extends AppCompatActivity {
 
@@ -33,7 +26,7 @@ public class LearnWordActivity extends AppCompatActivity {
 
     private TextView word_tv, ex_sentence_tv, practice_foreign;
     private TextToSpeech tts;
-    private TextView word, example_sentence, practice_foreign;
+    private TextView word, example_sentence;
     private ImageButton btn_back, btn_word_pronunciation, btn_sentence_pronunciation;
     private CheckBox btn_bookmark;
     private ImageView word_pic;
@@ -67,6 +60,14 @@ public class LearnWordActivity extends AppCompatActivity {
         word = (TextView) findViewById(R.id.word);
         word.setText(text);
 
+        //전달 받은 uri 로 UI 바인딩
+        word_pic = (ImageView) findViewById(R.id.word_pic);
+        word_pic.setImageURI(uri);
+
+        //전달 받은 exam 로 UI 바인딩
+        example_sentence = (TextView) findViewById(R.id.ex_sentence);
+        example_sentence.setText(exam);
+
         //단어 스피커 이미지버튼 클릭 리스너
         btn_word_pronunciation = (ImageButton) findViewById(R.id.btn_word_pronunciation);
         btn_word_pronunciation.setOnClickListener(new View.OnClickListener() {
@@ -75,17 +76,6 @@ public class LearnWordActivity extends AppCompatActivity {
                 speakOut(word.getText().toString());
             }
         });
-
-        //전달 받은 uri 로 UI 바인딩
-        word_tv = (TextView) findViewById(R.id.word);
-        ex_sentence_tv = (TextView) findViewById(R.id.ex_sentence);
-
-        word_pic = (ImageView) findViewById(R.id.word_pic);
-        word_pic.setImageURI(uri);
-
-        //전달 받은 exam 로 UI 바인딩
-        example_sentence = (TextView) findViewById(R.id.example_sentence);
-        example_sentence.setText(exam);
 
         //예문 스피커 이미지버튼 클릭 리스너
         btn_sentence_pronunciation = (ImageButton) findViewById(R.id.btn_sentence_pronunciation);
@@ -104,29 +94,19 @@ public class LearnWordActivity extends AppCompatActivity {
         vocaDB = VocaDB.getInstance(this);
         mContext = getApplicationContext();
 
-        //sharedPreferences 에서 선택된 언어 가져오기
-        language = getSharedPreferences("Language", MODE_PRIVATE).getString("language", null);
-        setLanguageUI(language);    //선택된 언어에 맞춰 TextView 의 텍스트를 설정하는 함수 호출
-
-        //뒤로가기 버튼
-        btn_back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
-
+        //단어 추가
         class InsertRunnable implements Runnable {
             @Override
             public void run() {
                 Voca voca = new Voca();
-                voca.word = word_tv.getText().toString();
-                voca.ex_sentence = ex_sentence_tv.getText().toString();
+                voca.word = word.getText().toString();
+                voca.ex_sentence = example_sentence.getText().toString();
                 voca.picture_uri = uri.toString();
                 VocaDB.getInstance(mContext).vocaDao().insert(voca);
             }
         }
 
+        //단어 삭제
         class DeleteRunnable implements Runnable {
             @Override
             public void run() {
@@ -135,6 +115,7 @@ public class LearnWordActivity extends AppCompatActivity {
             }
         }
 
+        //북마크 버튼(단어 추가, 단어 삭제기능 연결)
         btn_bookmark.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -169,6 +150,14 @@ public class LearnWordActivity extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(), PracticeWordActivity.class);
                 intent.putExtra("word", word_tv.getText());
                 startActivity(intent);
+            }
+        });
+
+        //뒤로가기 버튼
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
             }
         });
     }
