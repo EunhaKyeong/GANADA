eee0bf46a12eacca95203943b6ec540b55cf6715
EunhaKyeong <kyeongeh424@gmail.com>
2021-12-11 00:46:20 +0900

    Remove: 파일 삭제

--- app/src/main/java/com/pProject/ganada/ExamParsingView.java
+++
@@ -1,9 +0,0 @@
-package com.pProject.ganada;
-
-import java.util.HashMap;
-
-public interface ExamParsingView {
-    void onParsingLoading();
-    void onParsingSuccess(HashMap hm);
-    void onParsingFail();
-}
